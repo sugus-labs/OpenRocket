3401c2b236f49d550d0796c9df30366303d34dd5
gmartinvela <gustavo.martin@opendeusto.es>
2013-07-23 14:03:45 +0200

    Repaired the format of the different variables to adapt to the standard format

--- OpenRocket.ino
+++ OpenRocket.ino
@@ -120,8 +120,8 @@ int md;
 // b5 is calculated in bmp085GetTemperature(...), this variable is also used in bmp085GetPressure(...)
 // so ...Temperature(...) must be called before ...Pressure(...).
 long b5; 
-short temperature;
-long pressure;
+float temperature;
+float pressure;
 float atm; // "standard atmosphere"
 float alti; 
 
@@ -382,29 +382,29 @@ void loop()
     alti = calcAltitude(pressure);
     //dtostrf(floatVar, minStringWidthIncDecimalPoint, numVarsAfterDecimal, charBuf);
     Serial.print("Temp: ");
-    Serial.print(temperature);
+    Serial.print(temperature, 2);
     Serial.println(" C");
     Serial.print("Pres: ");
-    Serial.print(pressure);
-    Serial.println(" hPa");
+    Serial.print(pressure, 0);
+    Serial.println(" Pa");
     Serial.print("Atmophere: ");
-    Serial.println(atm);
+    Serial.println(atm, 4);
     Serial.print("Altitude: ");
-    Serial.print(alti);
+    Serial.print(alti, 2);
     Serial.println("M");
     Serial.println();
 
-    if (dataFile) {
-      dataFile.print(timestamp);
-      dataFile.print(",");
-      dataFile.print(temperature);
-      dataFile.print(",");
-      dataFile.println(pressure);
-    }  
-    // if the file isn't open, pop up an error:
-    else {
-      Serial.println("error opening datalog.txt");
-    }
+//    if (dataFile) {
+//      dataFile.print(timestamp);
+//      dataFile.print(",");
+//      dataFile.print(temperature);
+//      dataFile.print(",");
+//      dataFile.println(pressure);
+//    }  
+//    // if the file isn't open, pop up an error:
+//    else {
+//      Serial.println("error opening datalog.txt");
+//    }
     to_save ++;
   } 
   
@@ -416,7 +416,6 @@ void loop()
   }
 }
 
-// BMP085
 // Stores all of the bmp085's calibration values into global variables
 // Calibration values are required to calculate temp and pressure
 // This function should be called at the beginning of the program
@@ -434,123 +433,145 @@ void bmp085Calibration()
   mc = bmp085ReadInt(0xBC);
   md = bmp085ReadInt(0xBE);
 }
-// Calculate temperature given ut.
-// Value returned will be in units of 0.1 deg C
-short bmp085GetTemperature(unsigned int ut)
-{
+
+// Calculate temperature in deg C
+float bmp085GetTemperature(unsigned int ut){
   long x1, x2;
+
   x1 = (((long)ut - (long)ac6)*(long)ac5) >> 15;
   x2 = ((long)mc << 11)/(x1 + md);
   b5 = x1 + x2;
-  return ((b5 + 8)>>4);
+
+  float temp = ((b5 + 8)>>4);
+  temp = temp /10;
+
+  return temp;
 }
+
 // Calculate pressure given up
 // calibration values must be known
 // b5 is also required so bmp085GetTemperature(...) must be called first.
 // Value returned will be pressure in units of Pa.
-long bmp085GetPressure(unsigned long up)
-{
+long bmp085GetPressure(unsigned long up){
   long x1, x2, x3, b3, b6, p;
   unsigned long b4, b7;
+
   b6 = b5 - 4000;
   // Calculate B3
   x1 = (b2 * (b6 * b6)>>12)>>11;
   x2 = (ac2 * b6)>>11;
   x3 = x1 + x2;
   b3 = (((((long)ac1)*4 + x3)<<OSS) + 2)>>2;
+
   // Calculate B4
   x1 = (ac3 * b6)>>13;
   x2 = (b1 * ((b6 * b6)>>12))>>16;
   x3 = ((x1 + x2) + 2)>>2;
   b4 = (ac4 * (unsigned long)(x3 + 32768))>>15;
+
   b7 = ((unsigned long)(up - b3) * (50000>>OSS));
   if (b7 < 0x80000000)
     p = (b7<<1)/b4;
   else
     p = (b7/b4)<<1;
+
   x1 = (p>>8) * (p>>8);
   x1 = (x1 * 3038)>>16;
   x2 = (-7357 * p)>>16;
   p += (x1 + x2 + 3791)>>4;
+
   return p;
 }
+
 // Read 1 byte from the BMP085 at 'address'
 char bmp085Read(unsigned char address)
 {
   unsigned char data;
+
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(address);
   Wire.endTransmission();
+
   Wire.requestFrom(BMP085_ADDRESS, 1);
-  while(!Wire.available());
+  while(!Wire.available())
+    ;
+
   return Wire.read();
 }
+
 // Read 2 bytes from the BMP085
 // First byte will be from 'address'
 // Second byte will be from 'address'+1
 int bmp085ReadInt(unsigned char address)
 {
   unsigned char msb, lsb;
+
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(address);
   Wire.endTransmission();
+
   Wire.requestFrom(BMP085_ADDRESS, 2);
-  while(Wire.available()<2);
+  while(Wire.available()<2)
+    ;
   msb = Wire.read();
   lsb = Wire.read();
+
   return (int) msb<<8 | lsb;
 }
+
 // Read the uncompensated temperature value
-unsigned int bmp085ReadUT()
-{
+unsigned int bmp085ReadUT(){
   unsigned int ut;
+
   // Write 0x2E into Register 0xF4
   // This requests a temperature reading
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(0xF4);
   Wire.write(0x2E);
   Wire.endTransmission();
+
   // Wait at least 4.5ms
   delay(5);
+
   // Read two bytes from registers 0xF6 and 0xF7
   ut = bmp085ReadInt(0xF6);
   return ut;
 }
+
 // Read the uncompensated pressure value
-unsigned long bmp085ReadUP()
-{
+unsigned long bmp085ReadUP(){
+
   unsigned char msb, lsb, xlsb;
   unsigned long up = 0;
+
   // Write 0x34+(OSS<<6) into register 0xF4
   // Request a pressure reading w/ oversampling setting
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(0xF4);
   Wire.write(0x34 + (OSS<<6));
   Wire.endTransmission();
+
   // Wait for conversion, delay time dependent on OSS
   delay(2 + (3<<OSS));
+
   // Read register 0xF6 (MSB), 0xF7 (LSB), and 0xF8 (XLSB)
-  Wire.beginTransmission(BMP085_ADDRESS);
-  Wire.write(0xF6);
-  Wire.endTransmission();
-  Wire.requestFrom(BMP085_ADDRESS, 3);
-  // Wait for data to become available
-  while(Wire.available() < 3);
-  msb = Wire.read();
-  lsb = Wire.read();
-  xlsb = Wire.read();
+  msb = bmp085Read(0xF6);
+  lsb = bmp085Read(0xF7);
+  xlsb = bmp085Read(0xF8);
+
   up = (((unsigned long) msb << 16) | ((unsigned long) lsb << 8) | (unsigned long) xlsb) >> (8-OSS);
+
   return up;
 }
 
-void writeRegisterBMP085(int deviceAddress, byte address, byte val) {
+void writeRegister(int deviceAddress, byte address, byte val) {
   Wire.beginTransmission(deviceAddress); // start transmission to device 
   Wire.write(address);       // send register address
   Wire.write(val);         // send value to write
   Wire.endTransmission();     // end transmission
 }
 
-int readRegisterBMP085(int deviceAddress, byte address){
+int readRegister(int deviceAddress, byte address){
 
   int v;
   Wire.beginTransmission(deviceAddress);
@@ -568,10 +589,12 @@ int readRegisterBMP085(int deviceAddress, byte address){
 }
 
 float calcAltitude(float pressure){
+
   float A = pressure/101325;
   float B = 1/5.25588;
   float C = pow(A,B);
   C = 1 - C;
   C = C /0.0000225577;
+
   return C;
 }
